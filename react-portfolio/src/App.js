995ba6794531ae4f0e5c855819f35d04abba09ce
filthyningen <malikliddell@gmail.com>
2023-01-21 09:45:09 -0500

    FINALLY got tailwindcss config file to work.

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react'
 import Navbar from "./components/Navbar"
 
 function App() {
-  return (
-    <>
-      <Navbar />
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+		</>
+	)
 }
 
-export default App;
+export default App
