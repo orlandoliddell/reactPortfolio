7bc9106fb0456e9dc85234aa68f203ace52cd211
filthyningen <malikliddell@gmail.com>
2023-01-21 19:55:18 -0500

    navbar icons will easein and out a list of options to direct you around the website.

--- react-portfolio/src/components/Navbar.jsx
+++ react-portfolio/src/components/Navbar.jsx
@@ -9,18 +9,28 @@ const Navbar = () => {
 
 	return (
 		<div className="text-white flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4">
-			<h1 className="w-full text-3xl text-[rgb(212,223,0)] font-bold ">
+			<h1 className="w-full text-3xl text-[rgb(212,223,0)] font-bold m-4">
 				PORTFOLIO
 			</h1>
-			<ul className="flex hidden">
+			<ul className="hidden md:flex">
 				<li className="p-4">Home</li>
 				<li className="p-4">About</li>
 				<li className="p-4">Projects</li>
 			</ul>
-			<div onClick={handleNav}>
-                { !nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
+			<div onClick={handleNav} className="block md:hidden">
+				{!nav ? (
+					<AiOutlineClose size={20} />
+				) : (
+					<AiOutlineMenu size={20} />
+				)}
 			</div>
-			<div className="fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-400 bg-black">
+			<div
+				className={
+					!nav
+						? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-400 bg-black ease-in-out duration-500"
+						: "fixed left-[100%] top-0 w-[60%] h-full border-r border-r-gray-400 bg-black ease-in-out duration-500"
+				}
+			>
 				<h1 className="w-full text-3xl text-[rgb(212,223,0)] font-bold m-4">
 					PORTFOLIO
 				</h1>
