a42af458dd4f926e5efe4fdb83721019fd4bc63e
filthyningen <malikliddell@gmail.com>
2023-01-21 21:37:04 -0500

    added react typed to display what i work with. html, css, js, react.

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import Hero from './components/Hero'
+import React from "react"
+import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 
 function App() {
 	return (
 		<>
 			<Navbar />
-      <Hero />
+			<Hero />
 		</>
 	)
 }
